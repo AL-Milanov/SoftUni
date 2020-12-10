64796297984bff7a22ee33cad95291dd1002784a
fanOfKnivess <alexandermilanov161195@gmail.com>
2020-12-10 22:04:12 +0200

    Adding more projects.

---
+++ FundamentalsExamPrep/08.TheImitationGame/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+
+namespace _08.TheImitationGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string encryptedMessage = Console.ReadLine();
+
+            string command = Console.ReadLine();
+
+            while (command != "Decode")
+            {
+                string[] cmdArgs = command.Split('|', StringSplitOptions.RemoveEmptyEntries);
+                string action = cmdArgs[0];
+
+                if (action == "Move")
+                {
+                    int length = int.Parse(cmdArgs[1]);
+                    string line = encryptedMessage.Substring(0, length);
+                    encryptedMessage = encryptedMessage.Remove(0, length);
+                    encryptedMessage += line;
+                }
+                else if (action == "Insert")
+                {
+                    int index = int.Parse(cmdArgs[1]);
+                    string symbol = cmdArgs[2];
+
+                    encryptedMessage = encryptedMessage.Insert(index, symbol);
+                }
+                else if (action == "ChangeAll")
+                {
+                    string substring = cmdArgs[1];
+                    string replacement = cmdArgs[2];
+
+                    encryptedMessage = encryptedMessage.Replace(substring, replacement);
+                }
+
+
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine($"The decrypted message is: {encryptedMessage}");
+        }
+    }
+}
---
+++ FundamentalsExamPrep/09.RageQuit/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09.RageQuit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(\D)+";
+            string numberPattern = @"\d+";
+
+            string input = Console.ReadLine();
+            Regex regex = new Regex(pattern);
+            Regex numberRegex = new Regex(numberPattern);
+
+            MatchCollection matches = regex.Matches(input);
+            MatchCollection numMatches = numberRegex.Matches(input);
+
+            List<string> numbers = new List<string>();
+
+            foreach (Match number in numMatches)
+            {
+                numbers.Add(number.Value.ToString());
+            }
+
+
+            List<string> uniqueSymbols = new List<string>();
+            StringBuilder sb = new StringBuilder();
+            uniqueSymbols.OrderBy(x => x);
+
+            foreach (Match match in matches)
+            {
+                if (match.Success)
+                {
+
+                    for (int i = 0; i < match.Length; i++)
+                    {
+                        char character = match.Value[i];
+                        string toUpper = character.ToString().ToUpper();
+
+                        if (!uniqueSymbols.Contains(toUpper))
+                        {
+                            uniqueSymbols.Add(toUpper);
+
+                        }
+                    }
+                    if (numbers.Count == 0)
+                    {
+                        sb.Append(match);
+                    }
+                    if (numbers.Count > 0)
+                    {
+
+                        int index = int.Parse(numbers[0]);
+
+                        for (int i = 0; i < index; i++)
+                        {
+                            sb.Append(match.ToString().ToUpper());
+                        }
+
+                        numbers.RemoveAt(0);
+                    }
+                }
+            }
+
+            Console.WriteLine($"Unique symbols used: {uniqueSymbols.Count}");
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
