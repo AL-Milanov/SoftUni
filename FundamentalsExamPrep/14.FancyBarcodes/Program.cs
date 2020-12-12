4e6f89507c03a0295e0e34b73dbea672433ccbaf
fanOfKnivess <alexandermilanov161195@gmail.com>
2020-12-12 23:15:14 +0200

    adding new solutions.

---
+++ FundamentalsExamPrep/14.FancyBarcodes/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _14.FancyBarcodes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"@\#+[A-Z][A-Za-z0-9]{4,}[A-Z]@\#+";
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string barcode = Console.ReadLine();
+                bool doesntContainDigits = true;
+                Match match = Regex.Match(barcode, pattern);
+                
+                if (match.Success)
+                {
+                    string compare = match.Value;
+                    string productGroup = "";
+
+                    for (int j = 0; j < compare.Length; j++)
+                    {
+                        if (char.IsDigit(compare[j]))
+                        {
+                            doesntContainDigits = false;
+                            productGroup += compare[j];
+
+                        }
+                    }
+
+                    if (doesntContainDigits == true)
+                    {
+                        Console.WriteLine("Product group: 00");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Product group: {productGroup}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid barcode");
+                }
+            }
+        }
+    }
+}
---
+++ FundamentalsExamPrep/15.ExtractEmails/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _15.ExtractEmails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(?<=\s)([a-z]+|\d+)(\d+|\w+|\.+|-+)([a-z]+|\d+)\@[a-z]+\-?[a-z]+\.[a-z]+(\.[a-z]+)?";
+            string input = Console.ReadLine();
+
+            MatchCollection matches = Regex.Matches(input, pattern);
+
+            foreach (var item in matches)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
