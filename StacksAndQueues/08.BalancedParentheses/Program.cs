08b90908952f3ff1a1242668336c79f709f27860
fanOfKnivess <alexandermilanov161195@gmail.com>
2021-01-15 15:46:38 +0200

    Adding new projects in StacksAndQueues.

---
+++ StacksAndQueues/08.BalancedParentheses/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08.BalancedParentheses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<char> stack = new Stack<char>();
+            bool isMirrored = false;
+            
+            foreach (var item in input)
+            {
+                
+                if (item == '(' || item == '{' || item == '[')
+                {
+                    stack.Push(item);
+                }
+                else if (item == ')' || item == '}' || item == ']')
+                {
+                    if (stack.Any() == false)
+                    {
+                        isMirrored = false;
+                        break;
+                    }
+                    char currChar = stack.Peek();
+                    if (currChar == '(' && item == ')')
+                    {
+                        isMirrored = true;
+                        stack.Pop();
+                    }
+                    else if(currChar == '{' && item == '}')
+                    {
+                        isMirrored = true;
+                        stack.Pop();
+                    }
+                    else if (currChar == '[' && item == ']')
+                    {
+                        isMirrored = true;
+                        stack.Pop();
+                    }
+                    else
+                    {
+                        isMirrored = false;
+                        break;
+                    }
+                }
+            }
+            if (isMirrored)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
---
+++ StacksAndQueues/09.SimpleTextEditor/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09.SimpleTextEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfOperations = int.Parse(Console.ReadLine());
+            Stack<string> stack = new Stack<string>();
+            string text = string.Empty;
+
+            for (int i = 0; i < numberOfOperations; i++)
+            {
+                string command = Console.ReadLine();
+                string[] cmdArgs = command.Split();
+                string token = cmdArgs[0];
+                if (token == "1")
+                {
+                    stack.Push(text);
+                    text += cmdArgs[1];
+                }
+                else if (token == "2")
+                {
+                    int count = int.Parse(cmdArgs[1]);
+                    if (count <= text.Length)
+                    {
+                        stack.Push(text);
+                        for (int j = 0; j < count; j++)
+                        {
+                            text = text.Remove(text.Length - 1);
+                        }
+                    }
+                }
+                else if (token == "3")
+                {
+                    int index = int.Parse(cmdArgs[1]);
+                    if (index <= text.Length)
+                    {
+                        Console.WriteLine(text[index - 1]);
+                    }
+                }
+                else if (token == "4")
+                {
+                    text = stack.Peek();
+                    stack.Pop();
+                }
+
+            }
+        }
+    }
+}
