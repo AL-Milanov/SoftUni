d6a2e72aa323069629c3c07d78a8cfd0a7aff17b
fanOfKnivess <alexandermilanov161195@gmail.com>
2021-01-15 23:20:14 +0200

    Adding new project in Multidimensional Arrays folder.

---
+++ MultidimensionalArrays/02.2x2SquaresInMatrix/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._2x2SquaresInMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] rowsAndCols = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            string[,] matrix = new string[rowsAndCols[0], rowsAndCols[1]];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                string[] input = Console.ReadLine().Split();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = input[col];
+                }
+
+            }
+
+            int squaresOfEquals = 0;
+
+            for (int row = 0; row < matrix.GetLength(0) - 1; row++)
+            {
+
+                for (int col = 0; col < matrix.GetLength(1) - 1; col++)
+                {
+                    string currentChar = matrix[row, col];
+                    string nextChar = matrix[row, col + 1];
+                    if (currentChar == nextChar)
+                    {
+                        string bottomLeftChar = matrix[row + 1, col];
+                        string bottomRightChar = matrix[row + 1, col + 1];
+                        bool isSquareOfEquals = currentChar == bottomLeftChar && 
+                            nextChar == bottomRightChar;
+
+                        if (isSquareOfEquals)
+                        {
+                            squaresOfEquals++;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(squaresOfEquals);
+
+        }
+    }
+}
