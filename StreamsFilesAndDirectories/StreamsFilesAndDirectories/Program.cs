00d5094a458e6b8824c76b9e8c6446cd34d65ac5
fanOfKnivess <alexandermilanov161195@gmail.com>
2021-01-22 22:23:57 +0200

    Adding new folder StreamsFilesAndDirectories.

---
+++ StreamsFilesAndDirectories/StreamsFilesAndDirectories/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace StreamsFilesAndDirectories
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (StreamReader reader = new StreamReader("../../../text.txt"))
+            {
+                int counter = 0;
+                string line = reader.ReadLine();
+                Stack<string> stack = new Stack<string>();
+                
+                while (line != null)
+                {
+                    if (counter % 2 == 0)
+                    {
+                        if (line.Contains('-') || line.Contains(',') ||
+                             line.Contains(", ") || line.Contains('.') ||
+                             line.Contains('!') || line.Contains('?'))
+                        {
+                            line = line.Replace('-', '@');
+                            line = line.Replace(',', '@');
+                            line = line.Replace('.', '@');
+                            line = line.Replace('!', '@');
+                            line = line.Replace('?', '@');
+                        }
+                        stack = new Stack<string>(line.Split());
+                        Console.Write(string.Join(" ", stack));
+                        Console.WriteLine();
+                    }
+
+                    line = reader.ReadLine();
+                    counter++;
+
+                }
+            }
+        }
+    }
+}
