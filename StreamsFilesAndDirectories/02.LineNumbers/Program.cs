731e80d3ff38011b5aa2c81bdde30c22fea4df6e
fanOfKnivess <alexandermilanov161195@gmail.com>
2021-01-25 23:06:08 +0200

    Adding new project in StreamsFilesAndDirectories and renaming folder to 01.EvenLines.

---
+++ StreamsFilesAndDirectories/02.LineNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace _02.LineNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = File.ReadAllText("text.txt");
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                int lettersCount = 0;
+                int punctuationCount = 0;
+                foreach (var character in text)
+                {
+                    if (char.IsLetter(character))
+                    {
+                        lettersCount++;
+                    }
+                    else if (char.IsPunctuation(character))
+                    {
+                        punctuationCount++;
+                    }
+                }
+            }
+            File.WriteAllText("output.txt", text);
+        }
+    }
+}
