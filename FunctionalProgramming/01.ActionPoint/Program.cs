aa75579c5a5c8638fb4c5cd2f76142e7a8c3bf5c
fanOfKnivess <alexandermilanov161195@gmail.com>
2021-01-26 23:18:02 +0200

    Adding new folder FunctionalProgramming and new projects in it.

---
+++ FunctionalProgramming/01.ActionPoint/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _01.ActionPoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            Action<string> print = names => Console.WriteLine(names);
+            Array.ForEach(names, print);
+            
+        }
+
+    }
+}
---
+++ FunctionalProgramming/02.KnightsOfHonor/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _02.KnightsOfHonor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            Action<string> knights = names => Console.WriteLine($"Sir {names}");
+            Array.ForEach(names, knights);
+        }
+    }
+}
