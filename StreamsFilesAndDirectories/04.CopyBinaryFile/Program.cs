eeb3825bcc4f4ed16d72c4a3e2151cb948630bbe
fanOfKnivess <alexandermilanov161195@gmail.com>
2021-01-27 14:52:54 +0200

    Adding new project in StreamsFilesAndDirectories.

---
+++ StreamsFilesAndDirectories/04.CopyBinaryFile/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace _04.CopyBinaryFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (FileStream picture = new FileStream("copyMe.png", FileMode.Open, FileAccess.Read))
+            {
+                using (FileStream newCopy = new FileStream("newCopy.png",
+                FileMode.Create, FileAccess.Write))
+                {
+                    while (picture.Position != picture.Length)
+                    {
+                        byte[] buffer = new byte[4096];
+                        int count = picture.Read(buffer, 0, buffer.Length);
+                        newCopy.Write(buffer);
+                    }
+
+                }
+            }
+        }
+    }
+}
