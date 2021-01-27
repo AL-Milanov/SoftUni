5dd7482791b39087bc6227ee09af6e1e49635dea
fanOfKnivess <alexandermilanov161195@gmail.com>
2021-01-27 18:08:56 +0200

    Adding new project in StreamsFilesAndDirectories.

---
+++ StreamsFilesAndDirectories/06.ZipAndExtract/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace _06.ZipAndExtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ZipArchive zipArchive = ZipFile.Open("zipFile.zip", ZipArchiveMode.Create))
+            {
+                ZipArchiveEntry zipArchiveEntry = zipArchive.CreateEntryFromFile("copyMe.png", "copyMeInArchive.png");
+
+            }
+            ZipFile.ExtractToDirectory("zipFile.zip", "../../../../");
+        }
+    }
+}
