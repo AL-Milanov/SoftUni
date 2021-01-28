9a2b1074a4b8e44f5cca3189a1c1865070610d21
fanOfKnivess <alexandermilanov161195@gmail.com>
2021-01-28 21:05:03 +0200

    Adding new project in FunctionalProgramming.

---
+++ FunctionalProgramming/12.TriFunction/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _12.TriFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int sum = 0;
+            List<string> names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList();
+             Func<string, int, bool> function = 
+                    (names, number) => names.Sum(name => name) >= number;
+
+            List<string> bestName = names.Where(name => function(name, number)).ToList();
+            Console.WriteLine(bestName.First());
+        }
+    }
+}
