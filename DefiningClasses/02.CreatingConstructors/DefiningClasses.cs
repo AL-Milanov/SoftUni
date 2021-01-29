997b5e188af398f2a9174ca7c0ce3073c709259f
fanOfKnivess <alexandermilanov161195@gmail.com>
2021-01-29 23:37:34 +0200

    Adding project in DefiningClasses.

---
+++ DefiningClasses/02.CreatingConstructors/DefiningClasses.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person pesho = new Person();
+
+            Console.WriteLine(pesho.Name + " - " + pesho.Age);
+        }
+    }
+}
