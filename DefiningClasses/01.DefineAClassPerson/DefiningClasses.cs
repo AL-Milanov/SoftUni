ca854b0e97576c82a234dd6af886619ab2e64213
fanOfKnivess <alexandermilanov161195@gmail.com>
2021-01-29 23:18:09 +0200

    Adding new folder DefiningClasses and project in it.

---
+++ DefiningClasses/01.DefineAClassPerson/DefiningClasses.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person firstStudent = new Person();
+            Person secondStudent = new Person();
+            Person thirdStudent = new Person();
+
+            firstStudent.Name = "Pesho";
+            firstStudent.Age = 20;
+
+            secondStudent.Name = "Gosho";
+            secondStudent.Age = 18;
+
+            thirdStudent.Name = "Stamat";
+            thirdStudent.Age = 43;
+
+            Console.WriteLine(firstStudent.Name + "-" + firstStudent.Age);
+        }
+    }
+}
