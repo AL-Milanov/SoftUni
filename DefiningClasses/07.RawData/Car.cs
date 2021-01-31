1fddfa7cbd643129a575164e2964206ec3b0ec7b
fanOfKnivess <alexandermilanov161195@gmail.com>
2021-01-31 13:58:35 +0200

    Adding new project in DefiningClasses.

---
+++ DefiningClasses/07.RawData/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Car
+    {
+        public Car(string model, int engine, string cargoType, List<double> tires)
+        {
+            Model = model;
+            Engine = engine;
+            CargoType = cargoType;
+            Tires = tires;
+        }
+        public string Model { get; set; }
+        public int Engine { get; set; }
+        public string CargoType { get; set; }
+        public List<double> Tires { get; set; }
+    }
+}
---
+++ DefiningClasses/07.RawData/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DefiningClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Car> cars = new List<Car>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] car = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string model = car[0];
+                int enginePower = int.Parse(car[2]);
+                string cargoType = car[4];
+                List<double> tires = new List<double>() {
+                    double.Parse(car[5]),
+                    double.Parse(car[7]),
+                    double.Parse(car[9]),
+                    double.Parse(car[11])
+                };
+                cars.Add(new Car(model, enginePower, cargoType, tires));
+            }
+
+            string cargoTypeParameter = Console.ReadLine();
+
+            foreach (var car in cars)
+            {
+                if (car.CargoType == cargoTypeParameter)
+                {
+                    switch (cargoTypeParameter)
+                    {
+                        case "flamable":
+                            if (car.Engine > 250)
+                            {
+                                Console.WriteLine(car.Model);
+                            }
+                            break;
+
+                        case "fragile":
+                            bool isUnderPressure = false;
+                            foreach (var tire in car.Tires)
+                            {
+                                if (tire < 1)
+                                {
+                                    isUnderPressure = true;
+                                }
+                            }
+                            if (isUnderPressure)
+                            {
+                                Console.WriteLine(car.Model);
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
