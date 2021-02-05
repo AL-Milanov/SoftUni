a1d8df064f3641f9f2373ce98aa843e89556a7ed
fanOfKnivess <alexandermilanov161195@gmail.com>
2021-02-05 22:33:29 +0200

    Adding new folder Generics with homework projects.

---
+++ Generics/01.GenericBoxOfString/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _01.GenericBoxOfString
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                var data = Console.ReadLine();
+
+                Box<string> box = new Box<string>(data);
+                Console.WriteLine(box);
+            }
+        }
+    }
+}
---
+++ Generics/02.GenericBoxOfInteger/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _02.GenericBoxOfInteger
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                var data = int.Parse(Console.ReadLine());
+
+                Box<int> box = new Box<int>(data);
+                Console.WriteLine(box);
+            }
+        }
+    }
+}
