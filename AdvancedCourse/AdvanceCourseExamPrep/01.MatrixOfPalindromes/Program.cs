27a169347f8b9e057a72a318191d69185d9eccaf
fanOfKnivess <alexandermilanov161195@gmail.com>
2021-02-08 23:09:58 +0200

    Adding AdvanceCourseExamPrep folder and projects.

---
+++ AdvancedCourse/AdvanceCourseExamPrep/01.MatrixOfPalindromes/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.MatrixOfPalindromes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] alphabet = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+
+            int[] rowsAndCols = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int rows = rowsAndCols[0];
+            int cols = rowsAndCols[1];
+
+            string[,] matrix = new string[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+               
+                for (int col = 0; col < cols; col++)
+                {
+                    string input = $"{alphabet[row]}{alphabet[col + row]}{alphabet[row]}";
+
+                    matrix[row, col] = input;
+                }
+            }
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    Console.Write(matrix[row, col] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
