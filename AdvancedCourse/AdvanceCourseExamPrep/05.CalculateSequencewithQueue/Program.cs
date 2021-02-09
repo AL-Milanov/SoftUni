09c23d385aa44b3345d8213ad079d0e69e8cd1e3
fanOfKnivess <alexandermilanov161195@gmail.com>
2021-02-09 17:57:25 +0200

    Adding new projects in AdvanceCourseExamPrep.

---
+++ AdvancedCourse/AdvanceCourseExamPrep/05.CalculateSequencewithQueue/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.CalculateSequencewithQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long n = long.Parse(Console.ReadLine());
+
+            Queue<long> testQueue = new Queue<long>();
+            Queue<long> outputQueue = new Queue<long>();
+
+            testQueue.Enqueue(n);
+
+            int counter = 1;
+            for (int i = 1; i < 151; i++) //?
+            {
+                long number = testQueue.Peek();
+
+                if (counter == 1)
+                {
+                    testQueue.Enqueue(number + 1);
+                }
+                else if (counter == 2)
+                {
+                    number *= 2;
+                    number++;
+                    testQueue.Enqueue(number);
+                }
+                else if (counter == 3)
+                {
+                    number += 2;
+                    testQueue.Enqueue(number);
+                    outputQueue.Enqueue(testQueue.Peek());
+                    testQueue.Dequeue();
+                }
+                counter++;
+                if (counter == 4)
+                {
+                    counter = 1;
+                }
+            }
+
+
+            Console.WriteLine(string.Join(" ", outputQueue));
+        }
+    }
+}
