4ef7dbd3eb96b6c5ec6d1a91efb06761c2ecada0
fanOfKnivess <alexandermilanov161195@gmail.com>
2021-02-10 22:24:18 +0200

    Adding IteratorsAndComparators folder with homework project.

---
+++ AdvancedCourse/IteratorsAndComparators/CustomListyIterator/ListyIterator.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CustomListyIterator
+{
+    public class ListyIterator<T> : IEnumerable<T>
+    {
+        private readonly List<T> data;
+
+        private int index;
+
+        public ListyIterator()
+        {
+            this.data = new List<T>();
+            this.index = 0;
+        }
+        public ListyIterator(IEnumerable<T> data)
+        {
+            this.data = new List<T>(data);
+        }
+
+
+        public bool Move()
+        {
+            index++;
+            if (this.index >= data.Count)
+            {
+                this.index--;
+                return false;
+            }
+            return this.index < this.data.Count;
+        }
+
+        public void Print()
+        {
+            if (data.Count == 0 || index >= data.Count)
+            {
+                Console.WriteLine("Invalid Operation!");
+            }
+            else
+            {
+                Console.WriteLine(data[index]);
+            }
+        }
+
+        public bool HasNext()
+        {
+            int index = this.index + 1;
+            return index < this.data.Count;
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < this.data.Count; i++)
+            {
+                yield return this.data[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ AdvancedCourse/IteratorsAndComparators/CustomListyIterator/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CustomListyIterator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine()
+                 .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            ListyIterator<string> listOfData = new ListyIterator<string>(input.Skip(1));
+
+            string command = Console.ReadLine();
+            
+            while (command != "END")
+            {
+                if (command == "Move")
+                {
+                    Console.WriteLine(listOfData.Move());
+                }
+                else if (command == "Print")
+                {
+                    listOfData.Print();
+                }
+                else if (command == "HasNext")
+                {
+                    Console.WriteLine(listOfData.HasNext());
+                }
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
