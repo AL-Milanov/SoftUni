046cc91c5b9a763b9748da3bbc99e699bfad7e66
fanOfKnivess <alexandermilanov161195@gmail.com>
2021-02-10 23:08:08 +0200

    New homework project.

---
+++ AdvancedCourse/IteratorsAndComparators/02.Collection/ListyIterator.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace _02.Collection
+{
+    public class ListyIterator<T> : IEnumerable<T>
+    {
+        private readonly List<T> data;
+
+        private int index;
+
+        public ListyIterator()
+        {
+            this.data = new List<T>();
+            this.index = 0;
+        }
+        public ListyIterator(IEnumerable<T> data)
+        {
+            this.data = new List<T>(data);
+        }
+
+
+        public bool Move()
+        {
+            index++;
+            if (this.index >= data.Count)
+            {
+                this.index--;
+                return false;
+            }
+            return this.index < this.data.Count;
+        }
+
+        public void Print()
+        {
+            if (data.Count == 0 || index >= data.Count)
+            {
+                Console.WriteLine("Invalid Operation!");
+            }
+            else
+            {
+                Console.WriteLine(data[index]);
+            }
+        }
+
+        public bool HasNext()
+        {
+            int index = this.index + 1;
+            return index < this.data.Count;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < data.Count; i++)
+            {
+                T item = data[i];
+                yield return item;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ AdvancedCourse/IteratorsAndComparators/02.Collection/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.Collection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            ListyIterator<string> listyIterator = new ListyIterator<string>(input.Skip(1));
+
+            string command = Console.ReadLine();
+
+            while (command != "END")
+            {
+                if (command == "Move")
+                {
+                    Console.WriteLine(listyIterator.Move());
+                }
+                else if (command == "Print")
+                {
+                    listyIterator.Print();
+                }
+                else if (command == "HasNext")
+                {
+                    Console.WriteLine(listyIterator.HasNext());
+                }
+                else if (command == "PrintAll")
+                {
+                    foreach (var item in listyIterator)
+                    {
+                        Console.Write(item + " ");
+                    }
+                    Console.WriteLine();
+                }
+
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
