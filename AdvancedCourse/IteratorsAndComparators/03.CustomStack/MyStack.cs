fe68782860aea593e6001071bdda2950094d72e7
fanOfKnivess <alexandermilanov161195@gmail.com>
2021-02-11 23:04:56 +0200

    Adding new homework project in IteratorsAndComparators.

---
+++ AdvancedCourse/IteratorsAndComparators/03.CustomStack/MyStack.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03.CustomStack
+{
+    class MyStack<T> : IEnumerable<T>
+    {
+        private List<T> stack;
+
+        public MyStack()
+        {
+            stack = new List<T>();
+        }
+
+
+        public void Push(T element)
+        {
+            stack.Add(element);
+        }
+
+        public void Pop()
+        {
+            if (stack.Count > 0)
+            {
+                stack.RemoveAt(stack.Count - 1);
+            }
+            else 
+            {
+                throw new InvalidOperationException("No elements");
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = stack.Count - 1; i >= 0; i--)
+            {
+                yield return stack[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ AdvancedCourse/IteratorsAndComparators/03.CustomStack/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace _03.CustomStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStack<int> stack = new MyStack<int>();
+
+            string command = Console.ReadLine();
+
+            while (command != "END")
+            {
+                string[] cmdArgs = command
+                    .Split(new string[] { " ", ", " }, StringSplitOptions.RemoveEmptyEntries);
+
+                if (cmdArgs[0] == "Push")
+                {
+                    for (int i = 1; i < cmdArgs.Length; i++)
+                    {
+                        stack.Push(int.Parse(cmdArgs[i]));
+                    }
+                }
+                else if (cmdArgs[0] == "Pop")
+                {
+                    try
+                    {
+
+                        stack.Pop();
+
+                    }
+                    catch (Exception ex)
+                    {
+
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+
+
+                command = Console.ReadLine();
+            }
+
+
+            for (int i = 0; i < 2; i++)
+            {
+                foreach (var item in stack)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+    }
+}
