3f825e4e554b7ffd836b2d5818e4396fa5972832
fanOfKnivess <alexandermilanov161195@gmail.com>
2021-02-12 23:07:23 +0200

    Adding new homework projects in AdvanceCourseExamPrep.

---
+++ AdvancedCourse/AdvanceCourseExamPrep/03.Cooking/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.Cooking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> liquids = new Queue<int>(Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray());
+
+            Stack<int> ingredients = new Stack<int>(Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray());
+            int breadCount = 0;
+            int cakeCount = 0;
+            int pastryCount = 0;
+            int fruitPieCount = 0;
+
+
+            while (liquids.Count > 0 && ingredients.Count > 00)
+            {
+
+                int liquid = liquids.Dequeue();
+                int ingredient = ingredients.Peek();
+                int sum = liquid + ingredient;
+
+                bool isIncreased = false;
+                while (true)
+                {
+                    switch (sum)
+                    {
+                        case 25:
+                            breadCount++;
+                            isIncreased = true;
+                            break;
+                        case 50:
+                            cakeCount++;
+                            isIncreased = true;
+                            break;
+                        case 75:
+                            pastryCount++;
+                            isIncreased = true;
+                            break;
+                        case 100:
+                            fruitPieCount++;
+                            isIncreased = true;
+                            break;
+                    }
+
+                    ingredient += 3;
+
+                    if (isIncreased || liquids.Count == 0)
+                    {
+                        break;
+                    }
+                    liquid = liquids.Dequeue();
+                    sum = liquid + ingredient;
+
+                }
+                if (isIncreased)
+                {
+                    ingredients.Pop();
+                    continue;
+                }
+
+                ingredients.Pop();
+                ingredients.Push(ingredient);
+            }
+
+            bool isCompleteOrder = breadCount != 0 && cakeCount != 0 &&
+                                   fruitPieCount != 0 && pastryCount != 0;
+
+            if (isCompleteOrder)
+            {
+                Console.WriteLine("Wohoo! You succeeded in cooking all the food!");
+            }
+            else
+            {
+                Console.WriteLine("Ugh, what a pity! You didn't have enough materials to cook everything.");
+            }
+            string liquidsResult = "Liquids left: ";
+            string ingredientsResult = "Ingredients left: ";
+
+            liquidsResult += liquids.Count == 0 ? "none" : string.Join(", ", liquids);
+            ingredientsResult += ingredients.Count == 0 ? "none" : string.Join(", ", ingredients);
+
+            Console.WriteLine(liquidsResult);
+            Console.WriteLine(ingredientsResult);
+
+            Console.WriteLine($"Bread: {breadCount}");
+            Console.WriteLine($"Cake: {cakeCount}");
+            Console.WriteLine($"Fruit Pie: {fruitPieCount}");
+            Console.WriteLine($"Pastry: {pastryCount}");
+        }
+    }
+}
---
+++ AdvancedCourse/AdvanceCourseExamPrep/04.Selling/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+
+namespace _04.Selling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            char[,] matrix = new char[n, n];
+            int myRow = -1;
+            int myCol = -1;
+
+            int counter = 0;
+
+            int firstPillarRow = -1;
+            int firstPillarCol = -1;
+
+            int secondPillarRow = -1;
+            int secondPillarCol = -1;
+
+            for (int row = 0; row < n; row++)
+            {
+                string data = Console.ReadLine();
+                for (int col = 0; col < data.Length; col++)
+                {
+                    matrix[row, col] = data[col];
+                    if (matrix[row, col] == 'S')
+                    {
+                        myRow = row;
+                        myCol = col;
+                    }
+                    else if (matrix[row, col] == 'O')
+                    {
+                        if (counter == 0)
+                        {
+                            firstPillarRow = row;
+                            firstPillarCol = col;
+                            counter++;
+                        }
+                        else
+                        {
+                            secondPillarRow = row;
+                            secondPillarCol = col;
+                        }
+                    }
+                }
+            }
+
+            int moneyEarned = 0;
+
+            while (true)
+            {
+                string direction = Console.ReadLine();
+
+                matrix[myRow, myCol] = '-';
+
+                myRow = NewRow(direction, myRow, n);
+                myCol = NewCol(direction, myCol, n);
+
+                if (IsValid(myRow, myCol, n))
+                {
+                    if (char.IsDigit(matrix[myRow, myCol]))
+                    {
+                        moneyEarned += int.Parse(matrix[myRow, myCol].ToString());
+                    }
+                    else if (matrix[myRow, myCol] == 'O')
+                    {
+                        if (myRow == firstPillarRow && myCol == firstPillarCol)
+                        {
+                            myRow = secondPillarRow;
+                            myCol = secondPillarCol;
+                        }
+                        else if (myRow == secondPillarRow && myCol == secondPillarCol)
+                        {
+                            myRow = firstPillarRow;
+                            myCol = firstPillarCol;
+                        }
+                        matrix[firstPillarRow, firstPillarCol] = '-';
+                        matrix[secondPillarRow, secondPillarCol] = '-';
+                    }
+
+                    matrix[myRow, myCol] = 'S';
+                }
+
+                if (!IsValid(myRow, myCol, n) || moneyEarned >= 50)// ? money
+                {
+                    break;
+                }
+            }
+
+            if (moneyEarned >= 50)
+            {
+                Console.WriteLine("Good news! You succeeded in collecting enough money!");
+            }
+            else
+            {
+                Console.WriteLine("Bad news, you are out of the bakery.");
+            }
+            Console.WriteLine($"Money: {moneyEarned}");
+
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static bool IsValid(int row, int col, int n)
+        {
+            return row >= 0 && row < n && col >= 0 && col < n;
+        }
+
+        private static int NewCol(string direction, int col, int n)
+        {
+            if (direction == "left")
+            {
+                return col - 1;
+            }
+            else if (direction == "right")
+            {
+                return col + 1;
+            }
+
+            return col;
+        }
+
+        private static int NewRow(string direction, int row, int n)
+        {
+            if (direction == "up")
+            {
+                return row - 1;
+            }
+            else if (direction == "down")
+            {
+                return row + 1;
+            }
+
+            return row;
+        }
+    }
+}
