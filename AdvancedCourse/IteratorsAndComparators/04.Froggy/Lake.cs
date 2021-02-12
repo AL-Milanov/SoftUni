a095051518a6dcf19be38f414aeeb47e2d0d0f35
fanOfKnivess <alexandermilanov161195@gmail.com>
2021-02-12 09:37:08 +0200

    Adding new homework project in IteratorsAndComparators.

---
+++ AdvancedCourse/IteratorsAndComparators/04.Froggy/Lake.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _04.Froggy
+{
+    class Lake : IEnumerable<int>
+    {
+        private List<int> stones;
+
+        public Lake(int[] stones)
+        {
+            this.stones = stones.ToList();
+        }
+
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < stones.Count; i += 2)
+            {
+                yield return stones[i];
+            }
+
+            int index = stones.Count - 1;
+            index = index % 2 == 0 ?
+                 stones.Count - 2 : index;
+
+            for (int i = index; i >= 0; i -= 2)
+            {
+                yield return stones[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
