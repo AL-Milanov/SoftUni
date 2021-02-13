53d74190f07fa5b511ab4251294629b22a4abb70
fanOfKnivess <alexandermilanov161195@gmail.com>
2021-02-13 21:21:20 +0200

    Adding new practice project in AdvanceCourseExamPrep folder.

---
+++ AdvancedCourse/AdvanceCourseExamPrep/06.Opening/Bakery.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BakeryOpenning
+{
+    public class Bakery
+    {
+        private List<Employee> employees;
+
+        public Bakery(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+            this.employees = new List<Employee>();
+        }
+
+        public string Name { get; set; }
+
+        public int Capacity { get; set; }
+
+        public int Count => employees.Count;
+
+        public void Add(Employee employee)
+        {
+            if (this.employees.Count < Capacity)
+            {
+                this.employees.Add(employee);
+            }
+        }
+
+        public bool Remove(string name)// ?
+        {
+            Employee employee = employees.Where(n => n.Name == name).FirstOrDefault();
+
+            if (employee == null)
+            {
+                return false;
+            }
+
+            employees.Remove(employee);
+            return true;
+        }
+
+        public Employee GetOldestEmployee()
+        {
+            Employee employee = employees.OrderByDescending(a => a.Age).FirstOrDefault();
+
+            return employee;
+        }
+
+        public Employee GetEmployee(string name)
+        {
+            Employee employee = employees.Where(n => n.Name == name).FirstOrDefault();
+
+            return employee;
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Employees working at Bakery {this.Name}:");
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee}");
+            }
+            return sb.ToString();
+        }
+    }
+}
