ae1a9979e766b614036a6f1c488f0477df8208b4
fanOfKnivess <alexandermilanov161195@gmail.com>
2021-02-20 19:42:03 +0200

    Adding AdvanceExam folder.

---
+++ AdvancedCourse/AdvanceExam/P01/Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fieldSize = int.Parse(Console.ReadLine());
+            string[,] matrix = new string[fieldSize, fieldSize];
+
+            Queue<string> attacks = new Queue<string>(Console.ReadLine().Split(",").ToArray());
+
+            int firstPlayerShips = 0;
+            int secondPlayerShips = 0;
+            int firstPlayerDestroyed = 0;
+            int secondPlayerDestroyed = 0;
+
+            for (int row = 0; row < fieldSize; row++)
+            {
+                string[] data = Console.ReadLine().Split().ToArray();
+
+                for (int col = 0; col < fieldSize; col++)
+                {
+                    matrix[row, col] = data[col];
+                    if (matrix[row, col] == "<")
+                    {
+                        firstPlayerShips++;
+                    }
+                    else if (matrix[row, col] == ">")
+                    {
+                        secondPlayerShips++;
+                    }
+                }
+            }
+
+            int count = 0;
+            while (attacks.Count != 0)
+            {
+                string[] attackData = attacks.Dequeue().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                int rowAttack = int.Parse(attackData[0]);
+                int colAttack = int.Parse(attackData[1]);
+                int currentFirstDestroyed = 0;
+                int currentSecondDestroyed = 0;
+
+                if (IsValidCoordinate(rowAttack, colAttack, fieldSize))
+                {
+                    if (count % 2 == 0)
+                    {
+                        if (matrix[rowAttack, colAttack] == ">")
+                        {
+                            matrix[rowAttack, colAttack] = "X";
+                            secondPlayerShips--;
+                            firstPlayerDestroyed++;
+                        }
+                    }
+                    else
+                    {
+                        if (matrix[rowAttack, colAttack] == "<")
+                        {
+                            matrix[rowAttack, colAttack] = "X";
+                            firstPlayerShips--;
+                            secondPlayerDestroyed++;
+                        }
+                    }
+                    if (matrix[rowAttack, colAttack] == "#")
+                    {
+                        currentFirstDestroyed += IsFirstPlayerShip(rowAttack - 1, colAttack - 1, matrix);
+                        currentFirstDestroyed += IsFirstPlayerShip(rowAttack - 1, colAttack, matrix);
+                        currentFirstDestroyed += IsFirstPlayerShip(rowAttack - 1, colAttack + 1, matrix);
+                        currentFirstDestroyed += IsFirstPlayerShip(rowAttack, colAttack - 1, matrix);
+                        currentFirstDestroyed += IsFirstPlayerShip(rowAttack, colAttack + 1, matrix);
+                        currentFirstDestroyed += IsFirstPlayerShip(rowAttack + 1, colAttack - 1, matrix);
+                        currentFirstDestroyed += IsFirstPlayerShip(rowAttack + 1, colAttack, matrix);
+                        currentFirstDestroyed += IsFirstPlayerShip(rowAttack + 1, colAttack + 1, matrix);
+
+                        currentSecondDestroyed += IsSecondPlayerShip(rowAttack - 1, colAttack - 1, matrix);
+                        currentSecondDestroyed += IsSecondPlayerShip(rowAttack - 1, colAttack, matrix);
+                        currentSecondDestroyed += IsSecondPlayerShip(rowAttack - 1, colAttack + 1, matrix);
+                        currentSecondDestroyed += IsSecondPlayerShip(rowAttack, colAttack - 1, matrix);
+                        currentSecondDestroyed += IsSecondPlayerShip(rowAttack, colAttack + 1, matrix);
+                        currentSecondDestroyed += IsSecondPlayerShip(rowAttack + 1, colAttack - 1, matrix);
+                        currentSecondDestroyed += IsSecondPlayerShip(rowAttack + 1, colAttack, matrix);
+                        currentSecondDestroyed += IsSecondPlayerShip(rowAttack + 1, colAttack + 1, matrix);
+
+                        firstPlayerShips -= currentFirstDestroyed;
+                        secondPlayerShips -= currentSecondDestroyed;
+
+                        if (count % 2 == 0)
+                        {
+                            firstPlayerDestroyed += currentFirstDestroyed + currentSecondDestroyed;
+                        }
+                        else
+                        {
+                            secondPlayerDestroyed += currentFirstDestroyed + currentSecondDestroyed;
+                        }
+                    }
+                }
+
+                count++;
+
+                if (secondPlayerShips == 0)
+                {
+                    Console.WriteLine($"Player One has won the game! {firstPlayerDestroyed} ships have been sunk in the battle.");
+                    return;
+                }
+                else if (firstPlayerShips == 0)
+                {
+                    Console.WriteLine($"Player Two has won the game! {secondPlayerDestroyed} ships have been sunk in the battle.");
+                    return;
+                }
+            }
+
+            Console.WriteLine($"It's a draw! Player One has {firstPlayerShips} ships left. Player Two has {secondPlayerShips} ships left.");
+        }
+
+        private static int IsSecondPlayerShip(int row, int col, string[,] matrix)
+        {
+            if (IsValidCoordinate(row, col, matrix.GetLength(0)))
+            {
+                if (matrix[row, col] == ">")
+                {
+                    matrix[row, col] = "X";
+                    return 1;
+                }
+            }
+
+            return 0;
+        }
+
+        private static int IsFirstPlayerShip(int row, int col, string[,] matrix)
+        {
+            if (IsValidCoordinate(row, col, matrix.GetLength(0)))
+            {
+                if (matrix[row, col] == "<")
+                {
+                    matrix[row, col] = "X";
+                    return 1;
+                }
+            }
+
+            return 0;
+        }
+
+        private static bool IsValidCoordinate(int row, int col, int fieldSize)
+        {
+            return row >= 0 && row < fieldSize && col >= 0 && col < fieldSize;
+        }
+    }
+}
---
+++ AdvancedCourse/AdvanceExam/P02/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfWaves = int.Parse(Console.ReadLine());
+
+            Queue<int> defense = new Queue<int>(Console.ReadLine()
+                                                       .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                                       .Select(int.Parse)
+                                                       .ToArray());
+
+            Stack<int> orcs = new Stack<int>();
+            int count = 0;
+
+            for (int i = 0; i < numberOfWaves; i++)
+            {
+                count++;
+                orcs = new Stack<int>(Console.ReadLine()
+                                                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                                .Select(int.Parse)
+                                                .ToArray());
+
+                int orcAttack = orcs.Peek();
+                int defensePlate = defense.Peek();
+
+
+                if (count % 3 == 0)
+                {
+                    defense.Enqueue(int.Parse(Console.ReadLine()));
+                    count = 0;
+                }
+
+                while (defense.Count > 0 && orcs.Count > 0)
+                {
+
+                    if (orcAttack == defensePlate)
+                    {
+                        defensePlate -= orcAttack;
+                        orcs.Pop();
+                        defense.Dequeue();
+
+                        if (defense.Count > 0 && orcs.Count > 0)
+                        {
+                            orcAttack = orcs.Peek();
+                            defensePlate = defense.Peek();
+                        }
+
+                    }
+                    else if (orcAttack > defensePlate)
+                    {
+                        orcAttack -= defensePlate;
+                        defense.Dequeue();
+
+                        if (defense.Count > 0)
+                        {
+                            defensePlate = defense.Peek();
+                        }
+                    }
+                    else if (defensePlate > orcAttack)
+                    {
+                        defensePlate -= orcAttack;
+                        orcs.Pop();
+
+                        if (orcs.Count > 0)
+                        {
+                            orcAttack = orcs.Peek();
+
+                        }
+                    }
+
+                }
+
+                if (defense.Count == 0)
+                {
+
+                    orcs.Pop();
+                    orcs.Push(orcAttack);
+                    break;
+                }
+                if (defensePlate > 0)
+                {
+                    defense.Dequeue();
+                    defense = new Queue<int>(defense.Reverse());
+                    defense.Enqueue(defensePlate);
+                    defense = new Queue<int>(defense.Reverse());
+                }
+
+            }
+
+            if (orcs.Count == 0) //?
+            {
+                Console.WriteLine("The people successfully repulsed the orc's attack.");
+                Console.WriteLine($"Plates left: {String.Join(", ", defense)}");
+
+            }
+            else if (defense.Count == 0)
+            {
+                Console.WriteLine("The orcs successfully destroyed the Gondor's defense.");
+                Console.WriteLine($"Orcs left: {String.Join(", ", orcs)}");
+            }
+        }
+    }
+}
