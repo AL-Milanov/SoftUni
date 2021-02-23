18c17cdd4f63fd0dcb0865f85822f66d5264855b
fanOfKnivess <alexandermilanov161195@gmail.com>
2021-02-23 23:08:22 +0200

    Adding Inheritance homework.

---
+++ C#OOP/Inheritance/PlayersAndMonsters/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PlayersAndMonsters
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        
+        {
+            Knight darkKnight = new BladeKnight("Berav", 60);
+            Wizard darkWizard = new SoulMaster("Regalia", 80);
+            MuseElf hero = new MuseElf("Asmon", 120);
+            Console.WriteLine(hero);
+            Console.WriteLine(darkWizard);
+            Console.WriteLine(darkKnight);
+        }
+    }
+}
