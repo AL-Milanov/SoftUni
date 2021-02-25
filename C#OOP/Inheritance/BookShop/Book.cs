46e17df2c41a6144ea32bc35b5dead28b1fdf842
fanOfKnivess <alexandermilanov161195@gmail.com>
2021-02-25 22:44:02 +0200

    Adding new project.

---
+++ C#OOP/Inheritance/BookShop/Book.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookShop
+{
+    public class Book
+    {
+        private string author;
+        private string title;
+        private decimal price;
+        
+        public Book(string author, string title, decimal price)
+        {
+            this.Author = author;
+            this.Title = title;
+            this.Price = price;
+        }
+
+        public string Author 
+        {
+            get 
+            {
+                return this.author;
+            }
+            set
+            {
+                string[] authurName = value.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                if (authurName.Length == 2 && char.IsDigit(authurName[1][0]))
+                {
+                    throw new ArgumentException("Author not valid!");
+                }
+                this.author = value;
+            } 
+        }
+
+        public string Title 
+        { 
+            get 
+            { 
+                return this.title; 
+            } 
+            set 
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("Title not valid!");
+                }
+                this.title = value;
+            } 
+        }
+
+        public virtual decimal Price {
+            get
+            {
+                return this.price;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Price not valid!");
+                }
+                this.price = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Type: {GetType().Name}");
+            sb.AppendLine($"Title: {Title}");
+            sb.AppendLine($"Author: {Author}");
+            sb.AppendLine($"Price: {Price:f2}");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ C#OOP/Inheritance/BookShop/GoldenEditionBook.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookShop
+{
+    public class GoldenEditionBook : Book
+    {
+        private const decimal goldenBookIncrease = 1.3M;
+        public GoldenEditionBook(string author, string title, decimal price) 
+            : base(author, title, price)
+        {
+        }
+
+        public override decimal Price => base.Price * goldenBookIncrease;
+    }
+}
