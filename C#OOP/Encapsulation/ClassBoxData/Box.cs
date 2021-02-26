cd27eedd2b602371bb5b24928da74f64ef1682d4
fanOfKnivess <alexandermilanov161195@gmail.com>
2021-02-26 22:47:59 +0200

    Adding homework.

---
+++ C#OOP/Encapsulation/ClassBoxData/Box.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassBoxData
+{
+    //double length, double width, double height. 
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+
+
+        public double Length
+        {
+            get => this.length;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Length cannot be zero or negative.");
+                }
+
+                this.length = value;
+            }
+        }
+
+        public double Width
+        {
+            get => this.width;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Width cannot be zero or negative.");
+                }
+
+                this.width = value;
+            }
+        }
+
+        public double Height
+        {
+            get => this.height;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Height cannot be zero or negative.");
+                }
+
+                this.height = value;
+            }
+        }
+
+        public string GetSurfaceArea()
+        {
+            //2lw + 2lh + 2wh
+            double surfaceArea = (2 * Length * Width) + (2 * Length * Height) + (2 * Width * height);
+            return $"Surface Area - {surfaceArea:f2}";
+        }
+
+
+        public string GetLateralSurfaceArea()
+        {
+            //Lateral Surface Area = 2lh + 2wh
+            double lateralSurfaceArea = (2 * Length * Height) + (2 * Width * Height);
+            return $"Lateral Surface Area - {lateralSurfaceArea:f2}";
+        }
+
+        public string GetVolume()
+        {
+            //Volume = lwh
+            double volume = Length * Width * Height;
+            return $"Volume - {volume:f2}";
+        }
+    }
+}
---
+++ C#OOP/Encapsulation/ClassBoxData/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ClassBoxData
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                double lenght = double.Parse(Console.ReadLine());
+                double width = double.Parse(Console.ReadLine());
+                double height = double.Parse(Console.ReadLine());
+                Box box = new Box(lenght, width, height);
+
+                Console.WriteLine(box.GetSurfaceArea());
+                Console.WriteLine(box.GetLateralSurfaceArea());
+                Console.WriteLine(box.GetVolume());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
