605d7b7153d730623ed4abe9b90c9e57f4d8f271
fanOfKnivess <alexandermilanov161195@gmail.com>
2021-03-11 22:41:34 +0200

    homework

---
+++ C#OOP/InterfacesAndAbstraction/ExplicitInterfaces/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ExplicitInterfaces
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                string data = Console.ReadLine();
+
+                if (data == "End")
+                {
+                    break;
+                }
+
+                string[] dataSplited = data.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string name = dataSplited[0];
+                string country = dataSplited[1];
+                int age = int.Parse(dataSplited[2]);
+
+                Citizen citizen = new Citizen(name, country, age);
+
+                IPerson person = citizen;
+                person.GetName();
+                IResident resident = citizen;
+                resident.GetName();
+
+            }
+        }
+    }
+}
