34728f793dfe0befa197c7b869c7042871c3d94f
fanOfKnivess <alexandermilanov161195@gmail.com>
2021-03-29 17:58:23 +0300

    modified solution

--- C#OOP/OOPExamPrep/FestivalUnitTesting/FestivalManager.Tests/StageTests.cs
+++ C#OOP/OOPExamPrep/FestivalUnitTesting/FestivalManager.Tests/StageTests.cs
@@ -3,124 +3,147 @@
 // Test ONLY the Stage class. 
 namespace FestivalManager.Tests
 {
-    using FestivalManager.Entities;
     using NUnit.Framework;
     using System;
 
     [TestFixture]
-	public class StageTests
+    public class StageTests
     {
-		private const string FirstName = "Ivan";
-		private const string LastName = "Ivanov";
-		private const int LegitAge = 18;
-		private const string SongName = "TNT";
-		private Stage stage;
+        private const string FirstName = "Ivan";
+        private const string LastName = "Ivanov";
+        private const int LegitAge = 18;
+        private const string SongName = "TNT";
+        private Stage stage;
 
-		[SetUp]
-		public void SetUp()
+        [SetUp]
+        public void SetUp()
         {
-			stage = new Stage();
+            stage = new Stage();
         }
 
-		[Test]
-	    public void When_AddPerformerWithAgeLessThanEightteen_ShouldThrowArgumentException()
-	    {
-			Performer performer = new Performer(FirstName, LastName, 17);
-
-			Assert.Throws<ArgumentException>(() => stage.AddPerformer(performer));
-		}
-
-		[Test]
-		public void When_AddPerformerWithPerformerThatIsNull_ShouldThrowArgumentNullException()
+        [Test]
+        [TestCase(null, null)]
+        public void Test(Performer performer, Song song)
         {
-			Performer performer = null;
-			Assert.Throws<ArgumentNullException>(() => { stage.AddPerformer(performer); });
         }
 
-		[Test]
-		[TestCase(FirstName, LastName, LegitAge)]
-		[TestCase(FirstName, LastName, LegitAge * 2)]
-		public void When_AddPerformerWithCorrectData_ShouldAddPerformerToList(string firstName, string lastName, int age)
+        [Test]
+        public void When_AddPerformerWithAgeLessThanEightteen_ShouldThrowArgumentException()
         {
-			Performer performer = new Performer(firstName, lastName, age);
-			stage.AddPerformer(performer);
-			Assert.That(string.Join(Environment.NewLine, stage.Performers), Is.EqualTo(performer.ToString()));
+            Performer performer = new Performer(FirstName, LastName, 17);
+
+            Assert.Throws<ArgumentException>(() => stage.AddPerformer(performer));
         }
 
-		[Test]
-		public void When_AddPSongWithSongThatIsNull_ShouldThrowArgumentNullException()
-		{
-			Song song = null;
-			Assert.Throws<ArgumentNullException>(() => { stage.AddSong(song); });
-		}
-
-		[Test]
-		public void When_AddPSongWithSongThatIsLessThanOrOneMinute_ShouldThrowArgumentException()
-		{
-			TimeSpan duration = new TimeSpan(00, 00, 59);
-
-			Song firstSong = new Song(SongName, duration);
-
-			Assert.Throws<ArgumentException>(() => { stage.AddSong(firstSong); });
-		}
-
-		[Test]
-		public void When_AddSongToPerformerThatPerformerIsNotPresented_ShouldThrowArgumentException()
+        [Test]
+        public void When_AddPerformerWithPerformerThatIsNull_ShouldThrowArgumentNullException()
         {
-			Performer performer = new Performer(FirstName, LastName, LegitAge * 2);
-			Song song = new Song(SongName, new TimeSpan(00, 05, 23));
-			stage.AddPerformer(performer);
-			stage.AddSong(song);
-
-			Assert.Throws<ArgumentException>(() => { 
-				stage.AddSongToPerformer(SongName, "Invalid Name"); });
+            Performer performer = null;
+            Assert.Throws<ArgumentNullException>(() => { stage.AddPerformer(performer); });
         }
 
-		[Test]
-		public void When_AddSongToPerformerThatSongIsNotPresented_ShouldThrowArgumentException()
-		{
-			Performer performer = new Performer(FirstName, LastName, LegitAge * 2);
-			Song song = new Song(SongName, new TimeSpan(00, 05, 23));
-			stage.AddPerformer(performer);
-			stage.AddSong(song);
+        [Test]
+        [TestCase(FirstName, LastName, LegitAge)]
+        [TestCase(FirstName, LastName, LegitAge * 2)]
+        public void When_AddPerformerWithCorrectData_ShouldAddPerformerToList(string firstName, string lastName, int age)
+        {
+            Performer performer = new Performer(firstName, lastName, age);
+            stage.AddPerformer(performer);
+            Assert.That(string.Join(Environment.NewLine, stage.Performers), Is.EqualTo(performer.ToString()));
+        }
 
-			Assert.Throws<ArgumentException>(() => {
-				stage.AddSongToPerformer("Invalid Song", performer.FullName);
-			});
-		}
+        [Test]
+        public void When_AddPSongWithSongThatIsNull_ShouldThrowArgumentNullException()
+        {
+            Song song = null;
+            Assert.Throws<ArgumentNullException>(() => { stage.AddSong(song); });
+        }
 
-		[Test]
-		public void When_AddSongToPerformerWithCorrectData_ShouldAddSongToPerformer()
-		{
-			Performer performer = new Performer(FirstName, LastName, LegitAge * 2);
-			Song song = new Song(SongName, new TimeSpan(00, 05, 23));
-			stage.AddPerformer(performer);
-			stage.AddSong(song);
+        [Test]
+        public void When_AddPSongWithSongThatIsLessThanOrOneMinute_ShouldThrowArgumentException()
+        {
+            TimeSpan duration = new TimeSpan(00, 00, 59);
 
-			stage.AddSongToPerformer(SongName, performer.FullName);
+            Song firstSong = new Song(SongName, duration);
 
-			Assert.That(performer.SongList.Contains(song));
-		}
+            Assert.Throws<ArgumentException>(() => { stage.AddSong(firstSong); });
+        }
 
-		[Test]
-		[TestCase(1)]
-		[TestCase(5)]
-		public void When_Play_ShouldReturnStringWithPerformersCountAndSongListCount(int count )
+        [Test]
+        public void When_AddSongToPerformerThatPerformerIsNotPresented_ShouldThrowArgumentException()
+        {
+            Performer performer = new Performer(FirstName, LastName, LegitAge * 2);
+            Song song = new Song(SongName, new TimeSpan(00, 05, 23));
+            stage.AddPerformer(performer);
+            stage.AddSong(song);
+
+            Assert.Throws<ArgumentException>(() =>
+            {
+                stage.AddSongToPerformer(SongName, "Invalid Name");
+            });
+        }
+
+        [Test]
+        public void When_AddSongToPerformerThatSongIsNotPresented_ShouldThrowArgumentException()
+        {
+            Performer performer = new Performer(FirstName, LastName, LegitAge * 2);
+            Song song = new Song(SongName, new TimeSpan(00, 05, 23));
+            stage.AddPerformer(performer);
+            stage.AddSong(song);
+
+            Assert.Throws<ArgumentException>(() =>
+            {
+                stage.AddSongToPerformer("Invalid Song", performer.FullName);
+            });
+        }
+
+        [Test]
+        public void When_AddSongToPerformerWithCorrectData_ShouldAddSongToPerformer()
+        {
+            Performer performer = new Performer(FirstName, LastName, LegitAge * 2);
+            Song song = new Song(SongName, new TimeSpan(00, 05, 23));
+            stage.AddPerformer(performer);
+            stage.AddSong(song);
+
+            stage.AddSongToPerformer(SongName, performer.FullName);
+
+            Assert.That(performer.SongList.Contains(song));
+        }
+
+        [Test]
+        public void When_AddSongToPerformer_ShouldReturnString()
+        {
+            Performer performer = new Performer(FirstName, LastName, LegitAge * 2);
+            Song song = new Song(SongName, new TimeSpan(00, 05, 23));
+            stage.AddPerformer(performer);
+            stage.AddSong(song);
+
+            string actual = stage.AddSongToPerformer(SongName, performer.FullName);
+            string expected = $"{song} will be performed by {performer}";
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        [TestCase(1)]
+        [TestCase(5)]
+        public void When_Play_ShouldReturnStringWithPerformersCountAndSongListCount(int count)
         {
             for (int i = 0; i < count; i++)
             {
-				Performer performer = new Performer(FirstName, LastName, LegitAge + i);
+                Performer performer = new Performer(FirstName, LastName, LegitAge + i);
 
-				Song song = new Song(SongName, new TimeSpan(00, 05, 00));
+                Song song = new Song(SongName, new TimeSpan(00, 05, 00));
 
-				stage.AddPerformer(performer);
-				stage.AddSong(song);
+                stage.AddPerformer(performer);
+                stage.AddSong(song);
 
-				stage.AddSongToPerformer(SongName, performer.FullName);
+                stage.AddSongToPerformer(SongName, performer.FullName);
             }
-			string expectedMessage = $"{count} performers played {count} songs";
+            string expectedMessage = $"{count} performers played {count} songs";
 
-			Assert.That(stage.Play(), Is.EqualTo(expectedMessage));
+            Assert.That(stage.Play(), Is.EqualTo(expectedMessage));
         }
-	}
+
+    }
 }
