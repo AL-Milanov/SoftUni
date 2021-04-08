ad74329f53a0c593afe1950f540b5450e44ff985
fanOfKnivess <alexandermilanov161195@gmail.com>
2021-04-08 20:16:08 +0300

    Adding exam preparation

---
+++ C#OOP/OOPExamPrep/TheTankGame/TheTankGame/Core/Engine.cs
@@ -0,0 +1,43 @@
+﻿namespace TheTankGame.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Contracts;
+    using IO.Contracts;
+
+    public class Engine : IEngine
+    {
+        private bool isRunning;
+        private readonly IReader reader;
+        private readonly IWriter writer;
+        private readonly ICommandInterpreter commandInterpreter;
+
+        public Engine(
+            IReader reader, 
+            IWriter writer, 
+            ICommandInterpreter commandInterpreter)
+        {
+            this.reader = reader;
+            this.writer = writer;
+            this.commandInterpreter = commandInterpreter;
+
+            this.isRunning = false;
+        }
+
+        public void Run()
+        {
+            while (!this.isRunning)
+            {
+                string line = this.reader.ReadLine();
+                List<string> arguments = line.Split().ToList();
+
+                string output = this.commandInterpreter.ProcessInput(arguments);
+                this.writer.WriteLine(output);
+
+                this.isRunning = line.Contains("Terminate");
+            }
+
+        }
+    }
+}
