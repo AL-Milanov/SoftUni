89e3e1a649182a8828466f6fb7ae14c4cea2d138
fanOfKnivess <alexandermilanov161195@gmail.com>
2021-04-13 17:47:17 +0300

    new project

---
+++ AdvancedCourse/AdvanceCourseExamPrep/22.CryptoMaster/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    
+    static void Main()
+    {
+        var nums = Console.ReadLine()
+            .Split(new[] { ", " },
+                StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToList();
+
+        int seqLength = nums.Count;
+        int maxLength = 0;
+
+        for (int step = 1; step < seqLength; step++)
+        {
+            for (int stNode = 0; stNode < seqLength; stNode++)
+            {
+                var localMax = 1;
+
+                var currentElementIndex = stNode;
+                var nextElementIndex = (currentElementIndex + step) % seqLength;
+
+                while (nums[nextElementIndex] > nums[currentElementIndex])
+                {
+                    localMax++;
+
+                    currentElementIndex = nextElementIndex;
+                    nextElementIndex = (currentElementIndex + step) % seqLength;
+                }
+
+                if (maxLength < localMax)
+                {
+                    maxLength = localMax;
+                }
+            }
+        }
+
+        Console.WriteLine(maxLength);
+    }
+}
