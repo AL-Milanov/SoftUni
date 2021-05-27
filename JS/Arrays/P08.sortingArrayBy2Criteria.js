25913a60e9a453bbfc08fb01dd9e921a87cc111e
fanOfKnivess <alexandermilanov161195@gmail.com>
2021-05-27 22:41:58 +0300

    js homework.

---
+++ JS/Arrays/P08.sortingArrayBy2Criteria.js
@@ -0,0 +1,22 @@
+function solve(array){
+    array.sort((a, b) => a.length - b.length || a.localeCompare(b))
+    return array.join('\n');
+}
+
+console.log(solve(['alpha', 
+'beta', 
+'gamma']
+));
+console.log('-------------------------');
+console.log(solve(['Isacc', 
+'Theodor', 
+'Jack', 
+'Harrison', 
+'George']
+));
+console.log('________________________');
+console.log(solve(['test', 
+'Deny', 
+'omen', 
+'Default']
+));
