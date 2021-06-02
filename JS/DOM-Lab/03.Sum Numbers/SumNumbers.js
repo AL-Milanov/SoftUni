91848d02cdaac0f5fcafa8eb68edc2b72b0cf44f
fanOfKnivess <alexandermilanov161195@gmail.com>
2021-06-02 16:28:13 +0300

    DOM exercise

---
+++ JS/DOM-Lab/03.Sum Numbers/SumNumbers.js
@@ -0,0 +1,6 @@
+function calc() {
+    const firstNumber = Number(document.getElementById('num1').value);
+    const secondNumber = Number(document.getElementById('num2').value);
+    let sum = firstNumber + secondNumber; 
+    return document.querySelector('#sum').value = sum;;
+}
