12e415b696d8d9d53a6723f1c6ec3baf9fba053a
fanOfKnivess <alexandermilanov161195@gmail.com>
2021-06-04 22:43:11 +0300

    modified solution

--- JS/SyntaxFunctionsAndStatements/P04.timeToWalk.js
+++ JS/SyntaxFunctionsAndStatements/P04.timeToWalk.js
@@ -1,6 +1,17 @@
 function solve(steps, footprint, speedInKmh){
     let distance = steps * footprint;
 
+    let speedInMps = speedInKmh / 3.6;
+
+    let time = distance / speedInMps;
+    let breaks = Math.trunc(distance/ 500);
+
+    time += breaks * 60;
+    let hours = Math.trunc(time /3600);
+    let minutes = Math.trunc((time % 3600) / 60 );
+    let seconds = Math.round(time % 60);
+
+    return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
 }
 
-solve(4000, 0.60, 5);
+console.log(solve(4000, 0.60, 5));
