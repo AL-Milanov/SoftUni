16f4db8310ab5a6c2c64b04871d2c43c41eddbe5
fanOfKnivess <alexandermilanov161195@gmail.com>
2021-06-07 14:39:08 +0300

    DOM js exercise

---
+++ JS/DOM/05. Table - Search Engine/solution.js
@@ -0,0 +1,22 @@
+function solve() {
+   document.querySelector('#searchBtn').addEventListener('click', onClick);
+
+   function onClick() {
+      
+      let searchField = document.querySelector('#searchField');
+      let tableData = document.querySelectorAll('tbody tr');
+
+      for (const row of tableData) {
+         row.classList.remove('select');
+      }
+
+      let textInput = searchField.value; 
+
+      for (const row of tableData) {
+
+         if (row.textContent.includes(textInput)) {
+            row.classList.add('select');
+         }
+      }
+   }
+}
