e8d619af1395f8f70417ba9ee84596df78e40859
fanOfKnivess <alexandermilanov161195@gmail.com>
2021-06-15 23:55:41 +0300

    modified solution

--- JS/unit-testing/test/P04.mathEnforcer.test.js
+++ JS/unit-testing/test/P04.mathEnforcer.test.js
@@ -5,7 +5,7 @@ describe('mathEnforcer', () => {
     describe('addFive', () => {
         it('Should return undefined if its passed parameter different from number', () => {
             assert.equal(mathEnforcer.addFive('23'), undefined);
-            assert.equal(mathEnforcer.addFive(true), undefined);
+            assert.equal(mathEnforcer.addFive([1, 2, 3]), undefined);
         });
         it('Should return number + 5 if everything is correct', () => {
             let number = 5;
@@ -13,39 +13,36 @@ describe('mathEnforcer', () => {
             let floatingPointNumber = 5.5;
 
             assert.closeTo(mathEnforcer.addFive(number), (number + 5), 0.01);
-            assert.closeTo(mathEnforcer.addFive(negativeNumber), -5, 0.01);
-            assert.closeTo(mathEnforcer.addFive(floatingPointNumber), 10.5, 0.01);
+            assert.closeTo(mathEnforcer.addFive(negativeNumber), (negativeNumber + 5), 0.01);
+            assert.closeTo(mathEnforcer.addFive(floatingPointNumber), (floatingPointNumber + 5), 0.01);
         });
     });
     describe('subtractTen', () => {
         it('Should return undefined if its passed parameter different from number', () => {
             assert.equal(mathEnforcer.addFive('23'), undefined);
-            assert.equal(mathEnforcer.addFive(true), undefined);
+            assert.equal(mathEnforcer.addFive([1, 2, 3]), undefined);
         });
         it('Should return number - 10 if everything is correct', () => {
             let number = 5;
             let negativeNumber = -20;
             let floatingPointNumber = 5.5;
 
-            assert.closeTo(mathEnforcer.addFive(number), (number + 5), 0.01);
-            assert.closeTo(mathEnforcer.addFive(negativeNumber), (negativeNumber + 5), 0.01);
-            assert.closeTo(mathEnforcer.addFive(floatingPointNumber), (floatingPointNumber + 5), 0.01);
+            assert.closeTo(mathEnforcer.subtractTen(number), (number - 10), 0.01);
+            assert.closeTo(mathEnforcer.subtractTen(negativeNumber), (negativeNumber - 10), 0.01);
+            assert.closeTo(mathEnforcer.subtractTen(floatingPointNumber), (floatingPointNumber - 10), 0.01);
         });
     });
     describe('sum', () => {
-        it('Should return undefined if first of the parameters isnt number', () => {
+        it('Should return undefined if one of the parameters isnt number', () => {
             assert.equal(mathEnforcer.sum('23', 2), undefined);
             assert.equal(mathEnforcer.sum(true, 2), undefined);
-        });
-        it('Should return undefined if second of the parameters isnt number', () => {
-            assert.equal(mathEnforcer.sum(23, '2'), undefined);
-            assert.equal(mathEnforcer.sum(2, true), undefined);
+            assert.equal(mathEnforcer.sum(2, '2'), undefined);
+            assert.equal(mathEnforcer.sum(2, false), undefined);
         });
         it('Should return sum of both numbers if data inputs are correct', () => {
             assert.closeTo(mathEnforcer.sum(5, 5), 10, 0.01);
             assert.closeTo(mathEnforcer.sum(-5, 5), 0, 0.01);
             assert.closeTo(mathEnforcer.sum(5.53, 5.23), 10.76, 0.01);
-            assert.closeTo(mathEnforcer.sum(-5.53, -5.23), -10.76, 0.01);
         })
     })
 });
