ddebb4d16d92c7bc50efe25ab51554ad83191a3d
fanOfKnivess <alexandermilanov161195@gmail.com>
2021-06-18 16:03:07 +0300

    fixed Regex

--- JS/unit-testing/07.Form-Validation/formValidation.js
+++ JS/unit-testing/07.Form-Validation/formValidation.js
@@ -5,53 +5,59 @@ function validate() {
 
     let companyCheckbox = document.getElementById('company');
     let companyInfoFieldElement = document.getElementById('companyInfo');
-    let isValidCompany = true;
-
-    companyCheckbox.addEventListener('click', () => {
+    
+    companyCheckbox.addEventListener('change', () => {
         companyInfoFieldElement.style.display = companyCheckbox.checked ? 'block' : 'none';
     })
 
     function eventButtonHandler(e) {
         e.preventDefault();
-
+        
         let usernameInputElement = document.getElementById('username');
         let emailInputElement = document.getElementById('email');
         let passwordInputElement = document.getElementById('password');
         let confirmPasswordInputElement = document.getElementById('confirm-password');
-
-        let usernameRegex = /^[a-zA-z0-9]{3,20}$/;
-        let passwordRegex = /^\w{5,15}$/;
-        let emailRegex = /^.+\@.+\..+$/;
-
+        
+        let usernameRegex = /^[A-Za-z0-9]{3,20}$/;
+        let passwordRegex = /^[\w]{5,15}$/;
+        let emailRegex = /^[^@.]+@[^@]*\.[^@]*$/;
+        
         let isUsernameCorrect = usernameRegex.test(usernameInputElement.value);
         let isEmailCorrect = emailRegex.test(emailInputElement.value);
         let isPasswordsSame = passwordInputElement.value === confirmPasswordInputElement.value;
+        let isValidCompany = true;
         let isPasswordCorrect = false;
+
         if (passwordRegex.test(passwordInputElement.value)) {
             if (isPasswordsSame) {
                 isPasswordCorrect = true;
             }
         }
-
+        
         if (companyCheckbox.checked) {
             let companyNumberInput = document.getElementById('companyNumber');
             let number = Number(companyNumberInput.value);
             if (number < 1000 || number > 9999) {
                 isValidCompany = false;
-                elementBorder(companyNumberInput, isValidCompany);
+            } else {
+                isValidCompany = true;
             }
+            elementBorder(companyNumberInput, isValidCompany);
         }
-
+        let isPasswordValidAndCorrect = passwordRegex.test(passwordInputElement.value) && isPasswordCorrect;
         elementBorder(usernameInputElement, isUsernameCorrect);
         elementBorder(emailInputElement, isEmailCorrect);
-        elementBorder(passwordInputElement, passwordRegex.test(passwordInputElement.value));
-        elementBorder(confirmPasswordInputElement, passwordRegex.test(confirmPasswordInputElement.value));
+        elementBorder(passwordInputElement, isPasswordValidAndCorrect);
+
+        let isConfirmPasswordValid = passwordRegex.test(confirmPasswordInputElement.value) && isPasswordCorrect;
+
+        elementBorder(confirmPasswordInputElement, isConfirmPasswordValid);
 
         let everyThingIsRight = isUsernameCorrect && isEmailCorrect && isPasswordCorrect && isValidCompany;
         validDivElement.style.display = everyThingIsRight ? 'block' : 'none';
     }
 
     function elementBorder(element, isCorrect) {
-        element.style.borderColor = isCorrect ? 'none' : 'red';
+        element.style.borderColor = isCorrect ? '' : 'red';
     }
 }
