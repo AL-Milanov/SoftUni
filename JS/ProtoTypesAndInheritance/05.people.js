7d48d70925202dbbf3e8c444aeedd1ffd7b582d4
fanOfKnivess <alexandermilanov161195@gmail.com>
2021-06-21 10:26:26 +0300

    modified Manager and Senior class

--- JS/ProtoTypesAndInheritance/05.people.js
+++ JS/ProtoTypesAndInheritance/05.people.js
@@ -26,17 +26,40 @@ function solve() {
         }
     }
 
+    class Senior extends Employee {
+        constructor(name, age) {
+            super(name, age);
+            this.tasks.push('is working on a complicated task.');
+            this.tasks.push('is taking time off work.');
+            this.tasks.push('is supervising junior workers.');
+        }
+    }
+
+    class Manager extends Employee {
+        constructor(name, age){
+            super(name, age);
+            
+            this.divident = 0;
+            this.tasks.push('scheduled a meeting.');
+            this.tasks.push('is preparing a quarterly report.');
+        }
+
+        collectSalary(){
+            console.log(`${this.name} received ${this.salary + this.divident} this month.`);
+        }
+    }
 
     return {
         Employee,
         Junior,
-        //Senior,
-        //Manager,
+        Senior,
+        Manager,
     }
 }
 
 let classes = solve();
-const junior = new classes.Junior('Ivan', 25);
-
-console.log(junior.work());
-
+const junior = new classes.Manager('Ivan', 25);
+console.log(junior.hasOwnProperty('salary'));
+junior.salary = 500;
+junior.divident = 5000;
+console.log(junior.collectSalary());
