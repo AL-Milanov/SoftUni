c910f696c98d69b4162a6aa72ece6a55e69ab109
fanOfKnivess <alexandermilanov161195@gmail.com>
2021-07-26 23:15:15 +0300

    custom Queue

---
+++ C#/DataStructures/LinearData/01.FasterQueue/FastQueue.cs
@@ -0,0 +1,85 @@
+﻿namespace Problem01.FasterQueue
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class FastQueue<T> : IAbstractQueue<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public int Count { get; private set; }
+
+        public bool Contains(T item)
+        {
+            var current = this.head;
+
+            while (current != null)
+            {
+                if (current.Item.Equals(item))
+                {
+                    return true;
+                }
+
+                current = current.Next;
+            }
+
+            return false;
+        }
+
+        public T Dequeue()
+        {
+            EnsureNotEmpty();
+
+            T headToRemove = head.Item;
+
+            head = head.Next;
+            Count--;
+
+            return headToRemove;
+        }
+
+        public void Enqueue(T item)
+        {
+            Node<T> newNode = new Node<T>(item, null);
+
+            if (Count == 0)
+            {
+                tail = newNode;
+                head = tail;
+            } 
+            else
+            {
+                tail.Next = newNode;
+                tail = tail.Next;
+            }
+            Count++;
+        }
+
+        public T Peek()
+        {
+            EnsureNotEmpty();
+
+            return head.Item;
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            var current = this.head;
+
+            while (current != null)
+            {
+                yield return current.Item;
+                current = current.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+            => this.GetEnumerator();
+        private void EnsureNotEmpty()
+        {
+            if (this.Count == 0)
+                throw new InvalidOperationException();
+        }
+    }
+}
---
+++ C#/DataStructures/LinearData/01.FasterQueue/Program.cs
@@ -0,0 +1,14 @@
+﻿using Problem01.FasterQueue;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FastQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
