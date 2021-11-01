06c157f3aaf33b5a6150198e529e2baafd52468f
fanOfKnivess <alexandermilanov161195@gmail.com>
2021-11-01 17:21:06 +0200

    advanced LINQ homework

---
+++ C#/EntityFramework/LINQ/MusicHub/Data/Models/Album.cs
@@ -0,0 +1,33 @@
+﻿namespace MusicHub.Data.Models
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class Album
+    {
+        public Album()
+        {
+            Songs = new HashSet<Song>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(40)]
+        public string Name { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+        //TODO
+        public decimal Price => Songs.Select(s => s.Price).Sum();
+
+        [ForeignKey(nameof(Producer))]
+        public int? ProducerId { get; set; }
+        public virtual Producer Producer { get; set; }
+
+        public virtual ICollection<Song> Songs{ get; set; }
+    }
+}
