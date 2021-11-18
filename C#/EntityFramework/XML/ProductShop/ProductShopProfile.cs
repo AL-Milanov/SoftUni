8fc32ec90ccb2caffe29f9384457fbe3881a1d64
fanOfKnivess <alexandermilanov161195@gmail.com>
2021-11-18 23:27:21 +0200

    added new import data from xml methods and Dto models

---
+++ C#/EntityFramework/XML/ProductShop/ProductShopProfile.cs
@@ -0,0 +1,21 @@
+﻿namespace ProductShop
+{
+    using AutoMapper;
+    using ProductShop.DTOs.Import;
+    using ProductShop.Models;
+
+    public class ProductShopProfile : Profile
+    {
+        public ProductShopProfile()
+        {
+            //Import mappings
+            this.CreateMap<ImportUsersDto, User>()
+                .ForMember(x => x.Age,
+                           y => y.MapFrom(a => int.Parse(a.Age)));
+
+            this.CreateMap<ImportProductsDto, Product>();
+
+            this.CreateMap<ImportCategoriesDto, Category>();
+        }
+    }
+}
---
+++ C#/EntityFramework/XML/ProductShop/StartUp.cs
@@ -0,0 +1,83 @@
+﻿namespace ProductShop
+{
+    using AutoMapper;
+
+    using ProductShop.Data;
+    using ProductShop.DTOs.Import;
+    using ProductShop.Models;
+
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Xml.Linq;
+    using System.Xml.Serialization;
+
+    public class StartUp
+    {
+        private static readonly IConfigurationProvider configuration = new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile<ProductShopProfile>();
+        });
+
+        private static readonly IMapper mapper = new Mapper(configuration);
+
+        public static void Main(string[] args)
+        {
+            ProductShopContext context = new ProductShopContext();
+
+            //context.Database.EnsureDeleted();
+            //context.Database.EnsureCreated();
+
+            var inputXml = File.ReadAllText("Datasets/categories.xml");
+
+            Console.WriteLine(ImportCategories(context, inputXml));
+        }
+
+        public static string ImportUsers(ProductShopContext context, string inputXml)
+        {
+            var root = new XmlRootAttribute("Users");
+            var serializer = new XmlSerializer(typeof(List<ImportUsersDto>), root);
+
+            var deserializeUsersDto = (List<ImportUsersDto>)serializer.Deserialize(new StringReader(inputXml));
+
+            var users = mapper.Map<List<User>>(deserializeUsersDto);
+
+            context.Users.AddRange(users);
+            context.SaveChanges();
+
+            return $"Successfully imported {users.Count}";
+        }
+
+        public static string ImportProducts(ProductShopContext context, string inputXml)
+        {
+            var root = new XmlRootAttribute("Products");
+            var serializer = new XmlSerializer(typeof(List<ImportProductsDto>), root);
+
+            var deserializerProductsDto = (List<ImportProductsDto>)serializer.Deserialize(new StringReader(inputXml));
+
+            var products = mapper.Map<List<Product>>(deserializerProductsDto);
+
+            context.Products.AddRange(products);
+            context.SaveChanges();
+
+            return $"Successfully imported {products.Count}";
+        }
+
+        public static string ImportCategories(ProductShopContext context, string inputXml)
+        {
+            var root = new XmlRootAttribute("Categories");
+            var serializer = new XmlSerializer(typeof(List<ImportCategoriesDto>), root);
+
+            var deserializerCategoriesDto = (List<ImportCategoriesDto>)serializer
+                .Deserialize(new StringReader(inputXml));
+
+            var categories = mapper.Map<List<Category>>(deserializerCategoriesDto);
+
+            context.Categories.AddRange(categories);
+            context.SaveChanges();
+
+            return $"Successfully imported {categories.Count}";
+        }
+    }
+}
