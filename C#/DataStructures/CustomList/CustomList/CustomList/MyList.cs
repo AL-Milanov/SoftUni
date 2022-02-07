a29176060ed7d268123c7f2eac8b2dfdeefd8491
AL-Milanov <alexandermilanov161195@gmail.com>
2022-02-07 14:56:03 +0200

    List class with add method and indexer

---
+++ C#/DataStructures/CustomList/CustomList/CustomList/MyList.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomList
+{
+    public class MyList<T> : IEnumerable<T>
+    {
+        private T[] collection;
+
+        public int Capacity { get; private set; }
+
+        public int Count { get; private set; }
+
+        public T this[int index]
+        {
+            get
+            {
+                return collection[index];
+            }
+            set
+            {
+                if (index >= Capacity)
+                {
+                    throw new IndexOutOfRangeException("Index is outside of bounds of the array");
+                }
+
+                collection[index] = value;
+            }
+        }
+
+        public MyList(int length = 4)
+        {
+            collection = new T[length];
+            Capacity = length;
+        }
+
+        public MyList(IEnumerable<T> _collection)
+        {
+            Capacity = _collection.Count();
+
+            CopyCollection(_collection);
+        }
+
+        public void Add(T element)
+        {
+            if (element == null)
+                throw new ArgumentNullException($"{element} cannot be null");
+
+            Resize();
+
+            collection[Count++] = element;
+        }
+
+        private void Resize()
+        {
+            if (Count + 1 > Capacity)
+            {
+                Capacity *= 2;
+
+                CopyCollection(collection);
+            }
+        }
+
+        private void CopyCollection(IEnumerable<T> _collection)
+        {
+            Count = 0;
+
+            collection = new T[Capacity];
+
+            foreach (var item in _collection)
+            {
+                collection[Count++] = item;
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var item in collection)
+            {
+                if (item == null)
+                {
+                    break;
+                }
+
+                yield return item;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+            => GetEnumerator();
+    }
+}
---
+++ C#/DataStructures/CustomList/CustomList/CustomList/Program.cs
@@ -0,0 +1,23 @@
+﻿using CustomList;
+
+
+var list = new MyList<string>();
+
+for (int i = 0; i < 17; i++)
+{
+    list.Add($"element : {i}");
+}
+
+for (int i = 0; i < list.Count; i++)
+{
+    Console.WriteLine(list[i]);
+}
+
+list[1] = "hiii";
+
+Console.WriteLine("-----------------------------------------------------------");
+
+foreach (var item in list)
+{
+    Console.WriteLine(item);
+}
